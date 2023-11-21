b86f9467b878b24b98e85cf8af3c40160e2bd618
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2023-11-21 10:44:56 -0500

    Update database.php

--- MORENOCL/database.php
+++ MORENOCL/database.php
@@ -43,7 +43,7 @@
 		}
 		//---------------------------------------------------------
 			function connect($schu=null){
-				if (!is_null($schu)) { $name = "db".strtolower($schu); }else{ $name = "db".((isset(SCHU)) ? strtolower(SCHU) : '_qas'); }
+				if (!is_null($schu)) { $name = "db".strtolower($schu); }else{ $name = "db".strtolower(SCHU); }
 				$db_host = $this->$name;
 				//----------------------------------
 				if ($this->db_type == 'mysqli_') {
